f96c1bd8009a9fa44a81c9a4f22dba171adab5dc
Stanislav <77130336+standbit@users.noreply.github.com>
2022-01-08 19:31:49 +0300

    Get all information about passcard

--- main.py
+++ main.py
@@ -10,4 +10,7 @@ from datacenter.models import Passcard  # noqa: E402
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
-    print(Passcard.objects.all())
+    print('owner name:', Passcard.objects.all()[3].owner_name)
+    print('passcode:', Passcard.objects.all()[3].passcode)
+    print('created_at:', Passcard.objects.all()[3].created_at)
+    print('is_active:', Passcard.objects.all()[3].is_active)
