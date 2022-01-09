b2cb421ee32ee8555dbe5af7ef4f3fcc62a27203
Stanislav <77130336+standbit@users.noreply.github.com>
2022-01-09 16:40:05 +0300

    Get spent time in storage by visitor

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 
 import django
 from django.db.models.fields import NullBooleanField
+from django.utils.timezone import localtime
+import datetime
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
 from datacenter.models import Passcard, Visit  # noqa: E402
+
 
 if __name__ == '__main__':
     # Программируем здесь
@@ -18,4 +21,14 @@ if __name__ == '__main__':
     for visit in all_visits:
         if not visit.leaved_at:
             unfinished_visits.append(visit)
-    print(unfinished_visits)
+    now = localtime().replace(microsecond=0)
+    for visit in unfinished_visits:
+        entered_time = localtime(visit.entered_at)
+        delta = now - entered_time
+        seconds = int(delta.total_seconds())
+        hours = seconds // 3600
+        minutes = (seconds % 3600) // 60
+        secs = (seconds % 3600) % 60
+        a = datetime.timedelta(hours, minutes, secs)
+        print('Зашел в хранилище, время по Москве:\n', entered_time, '\n')
+        print('Находится в хранилище:\n', f'{hours}:{minutes}:{secs}')
